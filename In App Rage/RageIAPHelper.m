6ed5ffe545c9527ddcc0537d4a951b9ce8a3f8ef
AlexWu <ws169144@163.com>
2012-11-29 15:36:14 +0800

    添加测试源码

---
+++ In App Rage/RageIAPHelper.m
@@ -0,0 +1,27 @@
+//
+//  RageIAPHelper.m
+//  In App Rage
+//
+//  Created by Ray Wenderlich on 9/5/12.
+//  Copyright (c) 2012 Razeware LLC. All rights reserved.
+//
+
+#import "RageIAPHelper.h"
+
+@implementation RageIAPHelper
+
++ (RageIAPHelper *)sharedInstance {
+    static dispatch_once_t once;
+    static RageIAPHelper * sharedInstance;
+    dispatch_once(&once, ^{
+        //在这里输入你在应用里添加的书籍的product_ID
+        NSSet * productIdentifiers = [NSSet setWithObjects:
+                                      @"com.xinhuapinmei.xinhuatupianbao.testbook3",
+                                      @"com.xinhuapinmei.xinhuatupianbao.testbook4",
+                                      nil];
+        sharedInstance = [[self alloc] initWithProductIdentifiers:productIdentifiers];
+    });
+    return sharedInstance;
+}
+
+@end
